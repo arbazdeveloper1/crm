3314fea6149ad8d04735b9c56d332f0c457fffe3
Virang Jain <virangjain355@gmail.com>
2025-01-11 22:06:43 +0530

    One Controller

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -1,7 +1,25 @@
-import { cardInformationFunc, Price_Description } from "../models/bookingModel.js";
+import {  Price_Description } from "../models/bookingModel.js";
 
-export const priceDescription = async (req,res,next) => {
-    const { totalAmountQuoted, MCO_Description, AirlineName, TypeOfCharge, AirlineCost} = req.body;
+export const priceDescription = async (req,res) => {
+    const { 
+        totalAmountQuoted,
+        MCO_Description, 
+        AirlineName, 
+        TypeOfCharge, 
+        AirlineCost,
+        CardNumber,
+        Expiration,
+        Arl_Confirmation,
+        CardType,
+        CVV,
+        Email,
+        Currency,
+        Billing_Phone,
+        subjectLine,
+        cardHolderName,
+        TFN,
+        billingAddress
+    } = req.body;
     // console.log(req.body);
     try {
         const newTotalAmountQuoted =  totalAmountQuoted;
@@ -9,26 +27,6 @@ export const priceDescription = async (req,res,next) => {
         const newAirlineName =  AirlineName;
         const newTypeOfCharge =  TypeOfCharge;
         const newAirlineCost =  AirlineCost;
-        const sql = await Price_Description(newTotalAmountQuoted,newMCO_Description,newAirlineName,newTypeOfCharge,newAirlineCost);
-        if (sql) {
-            return res.status(201).json({
-                totalAmountQuoted: totalAmountQuoted,
-                MCO_Description: MCO_Description,
-                AirlineName: AirlineName,
-                TypeOfCharge: TypeOfCharge,
-                AirlineCost: AirlineCost
-            });
-        }next()
-        return res.status(400).json({message: "Not able to insert record" ,status:false});
-    } catch (error) {
-        console.log("Error in Bookings Controller: " + error.message);
-       return res.status(500).json({message: "Server Error"})
-    }
-}
-
-export const cardInformation = async (req, res) => {
-    const {CardNumber,Expiration,Arl_Confirmation,CardType,CVV,Email,Currency,Billing_Phone} = req.body;
-    try {
         const newCardNumber = CardNumber;
         const newExpiration = Expiration;
         const newArl_Confirmation = Arl_Confirmation;
@@ -37,13 +35,108 @@ export const cardInformation = async (req, res) => {
         const newEmail = Email;
         const newCurrency = Currency;
         const newBilling_Phone = Billing_Phone;
-        const sql = await cardInformationFunc(newCardNumber,newExpiration,newArl_Confirmation,newCardType,newCVV,newEmail,newCurrency,newBilling_Phone)
+        const newSubjectLine = subjectLine;
+        const newCardHolderName = cardHolderName;
+        const newTFN = TFN;
+        const newBillingAddress = billingAddress;
 
+
+        const sql = await Price_Description(
+            newTotalAmountQuoted,
+            newMCO_Description,
+            newAirlineName,
+            newTypeOfCharge,
+            newAirlineCost,
+            newCardNumber,
+            newExpiration,
+            newArl_Confirmation,
+            newCardType,
+            newCVV,
+            newEmail,
+            newCurrency,
+            newBilling_Phone,
+            newSubjectLine,
+            newCardHolderName,
+            newTFN,
+            newBillingAddress
+        );
+        if (sql) {
+            return res.status(201).json({
+                totalAmountQuoted: newTotalAmountQuoted,
+                MCO_Description: newMCO_Description,
+                AirlineName: newAirlineName,
+                TypeOfCharge: newTypeOfCharge,
+                AirlineCost: newAirlineCost,
+                CardNumber:newCardNumber,
+                Expiration:newExpiration,
+                Arl_Confirmation:newArl_Confirmation,
+                CardType:newCardType,
+                CVV:newCVV,
+                Email:newEmail,
+                Currency:newCurrency,
+                Billing_Phone:newBilling_Phone,
+                subjectLine:newSubjectLine,
+                cardHolderName:newCardHolderName,
+                TFN:newTFN,
+                billingAddress:newBillingAddress
+            });
+        }
+        return res.status(400).json({message: "Not able to insert record" ,status:false});
     } catch (error) {
-        console.log("Error in cardInformation Controller: " + error.message);
-        res.status(500).json({message: "Server Error"})
+        console.log("Error in Bookings Controller: " + error.message);
+       return res.status(500).json({message: "Server Error"})
     }
 }
-export const headerController = async (req,res) => {
-    const {subjectLine,cardHolderName,TFN,billingAddress}=req.body;
-}
+
+// export const cardInformation = async (req, res) => {
+//     const {CardNumber,Expiration,Arl_Confirmation,CardType,CVV,Email,Currency,Billing_Phone} = req.body;
+//     try {
+//         const newCardNumber = CardNumber;
+//         const newExpiration = Expiration;
+//         const newArl_Confirmation = Arl_Confirmation;
+//         const newCardType = CardType;
+//         const newCVV = CVV;
+//         const newEmail = Email;
+//         const newCurrency = Currency;
+//         const newBilling_Phone = Billing_Phone;
+//         const sql = await cardInformationFunc(newCardNumber,newExpiration,newArl_Confirmation,newCardType,newCVV,newEmail,newCurrency,newBilling_Phone)
+//         if(sql){
+//             return res.status(201).json({
+//                 CardNumber:newCardNumber,
+//                 Expiration:newExpiration,
+//                 Arl_Confirmation:newArl_Confirmation,
+//                 CardType:newCardType,
+//                 CVV:newCVV,
+//                 Email:newEmail,
+//                 Currency:newCurrency,
+//                 Billing_Phone:newBilling_Phone
+//             });
+//         }
+//         return res.status(400).json({message: "Not able to insert record", status:false});
+//     } catch (error) {
+//         console.log("Error in cardInformation Controller: " + error.message);
+//         res.status(500).json({message: "Server Error"})
+//     }
+// }
+// export const headerController = async (req,res) => {
+//     const {subjectLine,cardHolderName,TFN,billingAddress}=req.body;
+//     try {
+//         const newSubjectLine = subjectLine;
+//         const newCardHolderName = cardHolderName;
+//         const newTFN = TFN;
+//         const newBillingAddress = billingAddress;
+//         const sql = await Header(newSubjectLine,newCardHolderName,newTFN,newBillingAddress);
+//         if(sql){
+//             return res.status(201).json({
+//                 subjectLine:newSubjectLine,
+//                 cardHolderName:newCardHolderName,
+//                 TFN:newTFN,
+//                 billingAddress:newBillingAddress
+//             });
+//         }
+//         return res.status(400).json({message: "Not able to insert record", status:false});
+//     } catch (error) {
+//         console.log("Error in header Controller: "+ error.message);
+//         throw error;
+//     }
+// }
--- models/bookingModel.js
+++ models/bookingModel.js
@@ -1,26 +1,45 @@
 import { query } from '../config/db.js';
 
-export const Price_Description = async(newTotalAmountQuoted,newMCO_Description,newAirlineName,newTypeOfCharge,newAirlineCost) =>{
+export const Price_Description = async(
+    newTotalAmountQuoted,
+    newMCO_Description,
+    newAirlineName,
+    newTypeOfCharge,
+    newAirlineCost,
+    newCardNumber,
+    newExpiration,
+    newArl_Confirmation,
+    newCardType,
+    newCVV,
+    newEmail,
+    newCurrency,
+    newBilling_Phone,
+    newSubjectLine,
+    newCardHolderName,
+    newTFN,
+    newBillingAddress
+) =>{
     try {
-        const sql = await query("INSERT INTO form_data (total_amount = ? ,mco_description = ?,airline_names =?,charge_type = ?,airline_cost = ?",[newTotalAmountQuoted,newMCO_Description,newAirlineName,newTypeOfCharge,newAirlineCost]);
+        const sql = await query("INSERT INTO form_data (total_amount = ? ,mco_description = ?,airline_names =?,charge_type = ?,airline_cost = ?,card_number = ? ,expiration = ?,arl_confirmation=?, card_type = ?, cvv = ? ,email = ?, currency = ? ,  billing_phone = ?",
+        [newTotalAmountQuoted,newMCO_Description,newAirlineName,newTypeOfCharge,newAirlineCost,newCardNumber,newExpiration,newArl_Confirmation,newCardType,newCVV,newEmail,newCurrency,newBilling_Phone]);
         return sql;
     } catch (error) {
         console.log("Error in Inserting Data: " + error.message);
         throw error;
     }
 }
-export const cardInformationFunc = async (newCardNumber
-    ,newExpiration,newArl_Confirmation,newCardType,newCVV,newEmail,newCurrency,newBilling_Phone
-) => {
-    try {
-        const sql = await query("INSERT INTO form_data (card_number = ? ,expiration = ?,arl_confirmation=?, card_type = ?, cvv = ? ,email = ?, currency = ? ,  billing_phone = ?", [newCardNumber,newExpiration,newArl_Confirmation,newCardType,newCVV,newEmail,newCurrency,newBilling_Phone]);
-        return sql;
-    } catch (error) {
-        console.log("Error in CardInformation Controller:" + error.message);
-        throw error;
-    }
-}
-export const Header = async (params) => {
+// export const cardInformationFunc = async (newCardNumber
+//     ,newExpiration,newArl_Confirmation,newCardType,newCVV,newEmail,newCurrency,newBilling_Phone
+// ) => {
+//     try {
+//         const sql = await query("INSERT INTO form_data (card_number = ? ,expiration = ?,arl_confirmation=?, card_type = ?, cvv = ? ,email = ?, currency = ? ,  billing_phone = ?", [newCardNumber,newExpiration,newArl_Confirmation,newCardType,newCVV,newEmail,newCurrency,newBilling_Phone]);
+//         return sql;
+//     } catch (error) {
+//         console.log("Error in CardInformation Controller:" + error.message);
+//         throw error;
+//     }
+// }
+export const Header = async (newSubjectLine,newCardHolderName,newTFN,newBillingAddress) => {
     try {
         const sql = await query("INSERT INTO form_data (subject_line = ? , ")
     } catch (error) {
