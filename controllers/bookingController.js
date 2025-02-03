4f0bcf7cfd0e36b0733a07e0bdd16cb6e9102450
ravi <rv200414z@gmail.com>
2025-02-03 22:59:56 +0530

    file upload bug fix

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -2,17 +2,15 @@ import { Price_Description } from '../models/bookingModel.js'
 
 export const priceDescription = async (req, res) => {
     try {
-        console.log('request: ', req.body)
+        const FileName = req.files.map((item) => {
+            return item.filename;
+        });
+
+        req.FileName = FileName.join(",");
         const add_payload = req.body
-        const insert_resp = await Price_Description(add_payload)
+        const insert_resp = await Price_Description(add_payload, FileName)
         if (insert_resp) {
-            return res.status(201).json({
-                success: true,
-                message: 'Form Submitted successfully',
-                data: insert_resp,
-                redirect: '/dashboard',
-                reload: true
-            })
+            return res.status(201).json(insert_resp);
         }
         return res.status(400).json({ message: 'Not able to insert record', status: false })
     } catch (error) {
