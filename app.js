25568406cac82804aaf1aff15ea52d1eef2a5ce9
arbazdeveloper1 <arbaz.developer1@gmail.com>
2025-02-08 22:35:15 +0530

    push

--- app.js
+++ app.js
@@ -23,8 +23,9 @@ app.use(cookieParser());
 
 // Set static folder and views
 app.use(express.static(path.join(__dirname, 'public')));
-app.set('view engine', 'ejs');
+app.use('/uploads', express.static('uploads'));
 app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
 
 app.use('/public', express.static(path.join(__dirname, 'public')));
 
