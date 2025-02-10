afa05f69ffb0ea38799147b446c12900d1532d5a
ravi <rv200414z@gmail.com>
2025-02-10 13:33:05 +0530

    email functionality

--- routes/bookingRoutes.js
+++ routes/bookingRoutes.js
@@ -1,15 +1,25 @@
 import express, { Router } from "express";
-import { priceDescription, booking_list, new_booking_draft, AllBooking } from "../controllers/bookingController.js";
+import {
+  priceDescription,
+  booking_list,
+  new_booking_draft,
+  AllBooking,
+  EmailAcknowledge,
+} from "../controllers/bookingController.js";
 import upload from "../utils/multer.js";
-import multer from 'multer';
 import { verifyToken } from "../middlewares/authMiddleware.js";
 const route = express.Router();
-
-
 // Request for booking
-route.post("/make_booking",verifyToken, upload.array('file',10), priceDescription);
-route.get("/booking_list",verifyToken, booking_list)
-route.get("/new_booking_draft/:customer_id", verifyToken, new_booking_draft)
-route.get("/all_booking", verifyToken, AllBooking)
+route.post(
+  "/make_booking",
+  verifyToken,
+  upload.array("file", 10),
+  priceDescription
+);
+route.get("/booking_list", verifyToken, booking_list);
+route.get("/new_booking_draft/:customer_id", verifyToken, new_booking_draft);
+route.get("/all_booking", verifyToken, AllBooking);
+
+route.post("/api/send_email", verifyToken,EmailAcknowledge)
 
 export default route;
