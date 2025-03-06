bb0326773db1da95a28e0af2d3e17a2f100581ba
arbazdeveloper1 <arbaz.developer1@gmail.com>
2025-03-06 23:18:07 +0530

    up

--- routes/bookingRoutes.js
+++ routes/bookingRoutes.js
@@ -65,8 +65,5 @@ route.post('/api/Remarks', verifyToken, UpdateRemarks)
 route.post('/api/refund_booking', verifyToken,upload.array('file',10), RefundDescription);
 
 
-route.get('/booking_exchange', verifyToken, booking_exchange);
-route.get('/booking_refund', verifyToken, booking_refund);
-route.get('/booking_seatupgrade', verifyToken, booking_seatupgrade)
 
 export default route;
